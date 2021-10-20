e9bc157c2a1d5671a6abb3e59ce875798d34d176
Alexander Antonov <antonov.alexandar@icloud.com>
2021-10-20 18:24:17 +0300

    fixing detail page

---
+++ src/main/java/com/example/mobilele/models/view/OfferDetailView.java
@@ -0,0 +1,128 @@
+package com.example.mobilele.models.view;
+
+import com.example.mobilele.models.enums.Engine;
+import com.example.mobilele.models.enums.Transmission;
+
+import java.math.BigDecimal;
+
+public class OfferDetailView {
+
+    private Long id;
+    private String description;
+    private Engine engine;
+    private String imageUrl;
+    private Long mileage;
+    private BigDecimal price;
+    private Transmission transmission;
+    private Integer year;
+    private String created;
+    private String modified;
+    private String modelName;
+    private String sellerName;
+    private String brandName;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Engine getEngine() {
+        return engine;
+    }
+
+    public void setEngine(Engine engine) {
+        this.engine = engine;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
+    }
+
+    public Long getMileage() {
+        return mileage;
+    }
+
+    public void setMileage(Long mileage) {
+        this.mileage = mileage;
+    }
+
+    public BigDecimal getPrice() {
+        return price;
+    }
+
+    public void setPrice(BigDecimal price) {
+        this.price = price;
+    }
+
+    public Transmission getTransmission() {
+        return transmission;
+    }
+
+    public void setTransmission(Transmission transmission) {
+        this.transmission = transmission;
+    }
+
+    public Integer getYear() {
+        return year;
+    }
+
+    public void setYear(Integer year) {
+        this.year = year;
+    }
+
+    public String getCreated() {
+        return created;
+    }
+
+    public void setCreated(String created) {
+        this.created = created;
+    }
+
+    public String getModified() {
+        return modified;
+    }
+
+    public void setModified(String modified) {
+        this.modified = modified;
+    }
+
+    public String getModelName() {
+        return modelName;
+    }
+
+    public void setModelName(String modelName) {
+        this.modelName = modelName;
+    }
+
+    public String getSellerName() {
+        return sellerName;
+    }
+
+    public void setSellerName(String sellerName) {
+        this.sellerName = sellerName;
+    }
+
+    public String getBrandName() {
+        return brandName;
+    }
+
+    public void setBrandName(String brandName) {
+        this.brandName = brandName;
+    }
+}
+
